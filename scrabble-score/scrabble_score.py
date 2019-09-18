b7254204e86798703f7685fe3b3522d1b1e31b53
Ethan Che <eche@chicagobooth.edu>
2019-09-18 15:49:49 +0000

    scrabble_function_works

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,31 @@
 def score(word):
-    pass
+    # hardcoded scores to letters
+    scrabble_scores_to_letters = {
+            "1":['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+            "2":['D', 'G'],
+            "3":['B', 'C', 'M', 'P'],
+            "4":['F', 'H', 'V', 'W', 'Y'],
+            "5":['K'],
+            "8":['J', 'X'],
+            "10":['Q', 'Z']}
+    
+    # Create a dictionary of letters to scores
+    scrabble_letters_to_scores = {}
+    
+    for score_value in scrabble_scores_to_letters.keys():
+        letters_of_the_score = scrabble_scores_to_letters[score_value]
+        
+        for letter in letters_of_the_score:
+            letter_score = {letter: int(score_value)}
+            scrabble_letters_to_scores.update(letter_score)
+    
+    # Split word to characters
+    word = word.upper()
+    chars_of_word = [c for c in word]
+    
+    # Calculate the total score from "scrabble_letters_to_score"
+    total_scrabble_score = sum([scrabble_letters_to_scores[letter] for letter in word])
+    
+    return total_scrabble_score
+                               
+    
