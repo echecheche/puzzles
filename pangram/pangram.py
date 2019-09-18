c0d938592c57e49a598f9eaf2365a1a0d612296e
Ethan Che <eche@chicagobooth.edu>
2019-09-18 16:08:39 +0000

    is_pangram_finished

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,21 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    # Alphabet
+    alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    
+    # Lower case
+    sentence = sentence.lower()
+    # List of sorted letters
+    sentence_sorted = sorted(sentence)
+    # List of unique sorted letters
+    sentence_unique_letters = list(set(sentence_sorted))
+    # Strip punctuation and white space
+    chars_no_punc = [letter for letter in sentence_unique_letters if letter not in string.punctuation]
+    chars_no_punc_no_space = [letter for letter in chars_no_punc if letter != " "]
+    chars_no_punc_no_space.sort()
+    
+    # The output here should be the list of unique letters, excluding spaces and punctuation
+    is_pangram = alphabet == chars_no_punc_no_space
+    
+    return(is_pangram)
