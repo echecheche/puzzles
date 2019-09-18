c96ac714e0eaf43dbe1c5a2f8e7343809da526ee
Jinglin Wang <wangjinglin1996@gmail.com>
2019-09-18 20:07:50 +0000

    fixed collatez conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,16 +1,25 @@
 def steps(n):
+    
     count = 0
+
     while n != 1:
+
         print(count, n)
-        if (n % 2) == 0:
-            n = n/2
-            count += 1
+        
+        if (n == int(n) or (n < 0)) == False:
+            raise ValueError('not an integer or negative')
+            break
             
         else:
-            n = 3*n + 1
-            count += 1
+            if (n % 2) == 0:
+                n = n/2
+
+            else:
+                n = 3*n + 1
+
+        count += 1
             
-    print(count,n)
+    print(count,n)gi
 
 
         
